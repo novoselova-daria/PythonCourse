fe455de04ff9cad1710aabb962784c9031739861
novoselova-daria <48364546+novoselova-daria@users.noreply.github.com>
2019-03-09 01:35:40 +0300

    Add files via upload

---
+++ hw_A2.py
@@ -0,0 +1,10 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> n1= 'Hello'
+>>> n2= 'world!'
+>>> def f(n1, n2):
+	return ((n1+' '+n2)*10)
+
+>>> print (f(n1, n2))
+Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!Hello world!
+>>> 
